5503e816362665b1d14186a17e2207ce6c1a0d4f
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-07 18:13:12 +0530

    minor UI changes in layout

--- src/app/partial/partial-layout.routing.ts
+++ src/app/partial/partial-layout.routing.ts
@@ -4,5 +4,5 @@ import { Routes } from '@angular/router';
 export const PartialLayoutRoutes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadChildren: () => import('../partial/dashboard/dashboard.module').then(m => m.DashboardModule) },
-
+    { path: 'valve-list', loadChildren: () => import('../partial/valve-list/valve-list.module').then(m => m.ValveListModule) },
 ];
