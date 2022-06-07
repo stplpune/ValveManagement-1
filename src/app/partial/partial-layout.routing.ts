2700a863a305c4fe421df69b0d6293641f6a52e9
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2022-06-07 17:37:39 +0530

    navbar, sidebar and dashboard page design completed

--- src/app/partial/partial-layout.routing.ts
+++ src/app/partial/partial-layout.routing.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 
 
-//admin is user Role is 1, 2 for consultant , 3 for Bidder & 4 is bidder subscription
-
 export const PartialLayoutRoutes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadChildren: () => import('../partial/dashboard/dashboard.module').then(m => m.DashboardModule) },
