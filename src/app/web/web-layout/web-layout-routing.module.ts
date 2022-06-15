2d4e0b2fd0a7340ef6b5f445315c6e606933fa2a
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-06-15 19:09:02 +0530

    webLayout Changes

--- src/app/web/web-layout/web-layout-routing.module.ts
+++ src/app/web/web-layout/web-layout-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoAuthGuardService } from 'src/app/core/auth/no-auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('../../web/login/login.module').then(m => m.LoginModule) },];
+  { path: 'login', loadChildren: () => import('../../web/login/login.module').then(m => m.LoginModule) 
+  , canActivate: [NoAuthGuardService]},];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
