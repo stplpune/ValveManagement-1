23b8e4ccc0781f6c92a038fa371fa3a9d9552958
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2022-06-17 14:22:10 +0530

    dashboard changes

--- src/app/partial/dashboard/dashboard.component.ts
+++ src/app/partial/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/core/services/api.service';
+import { CommonService } from 'src/app/core/services/common.service';
+import { ErrorsService } from 'src/app/core/services/errors.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { LocalstorageService } from 'src/app/core/services/localstorage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  valveSummaryArray:any;
+
+  constructor(
+    public commonService: CommonService,
+    public apiService: ApiService,
+    private toastrService: ToastrService,
+    private errorSerivce: ErrorsService,
+    private spinner: NgxSpinnerService,
+    private localStorage: LocalstorageService,
+  ) { }
 
   ngOnInit(): void {
+    this.getValveSummary();
   }
 
+  getValveSummary() {
+    this.spinner.show();
+    this.apiService.setHttp('get', "ValveMaster/GetValveSummary?UserId=" + this.localStorage.userId(), false, false, false, 'valvemgt');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        if (res.statusCode === "200") {
+          this.spinner.hide();
+          this.valveSummaryArray = res.responseData;
+        } else {
+          this.spinner.hide();
+          this.valveSummaryArray = [];
+          this.commonService.checkDataType(res.statusMessage) == false ? this.errorSerivce.handelError(res.statusCode) : this.toastrService.error(res.statusMessage);
+        }
+      },
+      error: ((error: any) => { this.errorSerivce.handelError(error.status),this.spinner.hide(); })
+    });
+  }
+
+ 
 }
