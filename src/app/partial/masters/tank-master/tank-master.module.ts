1a4db80450af523b56fffe781859ba375c7fc8a5
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2023-01-13 17:17:02 +0530

    data fetch

--- src/app/partial/masters/tank-master/tank-master.module.ts
+++ src/app/partial/masters/tank-master/tank-master.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { TankMasterRoutingModule } from './tank-master-routing.module';
 import { TankMasterComponent } from './tank-master.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxSelectModule } from 'ngx-select-ex';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     TankMasterRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxSelectModule
   ]
 })
 export class TankMasterModule { }
