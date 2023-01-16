9b3ad01834b77990156a3f9eed62f644af1d2786
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2023-01-16 12:07:37 +0530

    validation done

--- src/app/partial/masters/tank-master/tank-master.module.ts
+++ src/app/partial/masters/tank-master/tank-master.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { TankMasterRoutingModule } from './tank-master-routing.module';
 import { TankMasterComponent } from './tank-master.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxSelectModule } from 'ngx-select-ex';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   declarations: [
     TankMasterComponent
@@ -14,7 +13,10 @@ import { NgxSelectModule } from 'ngx-select-ex';
     CommonModule,
     TankMasterRoutingModule,
     ReactiveFormsModule,
-    NgxSelectModule
+    NgxSelectModule,
+    NgxPaginationModule
+
+
   ]
 })
 export class TankMasterModule { }
