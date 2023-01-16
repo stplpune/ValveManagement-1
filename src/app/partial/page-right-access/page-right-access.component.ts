30fa5a4ab37c1be41240058c1d04c51dd1406139
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2023-01-16 18:40:09 +0530

    add page right access page

---
+++ src/app/partial/page-right-access/page-right-access.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-right-access',
+  templateUrl: './page-right-access.component.html',
+  styleUrls: ['./page-right-access.component.css']
+})
+export class PageRightAccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
