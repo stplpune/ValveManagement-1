9bf6717aa6eab2944234d134fc5965817b62ed97
vaishaliborate <vaishali.borate@shauryatechnosoft.com>
2023-01-17 15:35:40 +0530

    page right access changes

--- src/app/partial/page-right-access/page-right-access.module.ts
+++ src/app/partial/page-right-access/page-right-access.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { NgxSelectModule } from 'ngx-select-ex';
 import { PageRightAccessRoutingModule } from './page-right-access-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PageRightAccessComponent } from './page-right-access.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 @NgModule({
   declarations: [
-    // PageRightAccessComponent
+    PageRightAccessComponent
   ],
   imports: [
     CommonModule,
     PageRightAccessRoutingModule,
     NgxSelectModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule
   ]
 })
 export class PageRightAccessModule { }
