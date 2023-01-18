9a8ab2167f61ac4168984724814160b350c3219e
yogesh7355 <yogesh.jadhav@shauryatechnosoft.com>
2023-01-18 18:46:57 +0530

    filter changes commit

--- src/app/partial/masters/tank-calibration/tank-calibration.component.ts
+++ src/app/partial/masters/tank-calibration/tank-calibration.component.ts
@@ -64,8 +64,8 @@ export class TankCalibrationComponent implements OnInit {
 
   getFilterForm() {
     this.filterFrm = this.fb.group({
-      yojanaId: [0],
-      networkId: [0]
+      yojanaId: [''],
+      networkId: ['']
     })
   }
 
@@ -135,7 +135,7 @@ export class TankCalibrationComponent implements OnInit {
   getAllTankCalibration() {
     this.spinner.show();
     let filterValue = this.filterFrm.value;
-    let str = 'UserId=' + this.localstorageData.userId + '&pageno=' + this.pageNumber + '&pagesize=' + this.pagesize + '&YojanaId=' + filterValue.yojanaId + '&NetworkId=' + (filterValue.networkId || 0);
+    let str = 'UserId=' + this.localstorageData.userId + '&pageno=' + this.pageNumber + '&pagesize=' + this.pagesize + '&YojanaId=' + (filterValue.yojanaId || 0) + '&NetworkId=' + (filterValue.networkId || 0);
     this.apiService.setHttp('get', 'TankInfo/GetAllTankCalibration?' + str, false, false, false, 'valvemgt');
     this.apiService.getHttp().subscribe({
       next: (res: any) => {
@@ -171,14 +171,13 @@ export class TankCalibrationComponent implements OnInit {
 
   clearfilter(flag: any) {
     if (flag == 'yojana') {
-      this.filterFrm.controls['yojanaId'].setValue(0);
-      this.filterFrm.controls['networkId'].setValue(0);    
-    } else if (flag == 'network') {
-      this.filterFrm.controls['yojanaId'].setValue(this.filterFrm.value.yojanaId);
-      this.filterFrm.controls['networkId'].setValue(0);  
-    }
-    this.getAllTankCalibration();
-
+      this.filterFrm.controls['networkId'].setValue('');    
+    } 
+    // else if (flag == 'network') {
+    //   this.filterFrm.controls['yojanaId'].setValue(this.filterFrm.value.yojanaId);
+    //   this.filterFrm.controls['networkId'].setValue('');      
+    // }
+    
   }
 
   onSubmit() {
