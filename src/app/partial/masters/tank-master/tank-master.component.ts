6532bcd7cc7bd345cc5a2f6377be43174bc3a291
Vinayak-ShauryaTechnosoft <vinayak.bombale@shauryatechnosoft.com>
2023-01-31 14:26:35 +0530

    upload

--- src/app/partial/masters/tank-master/tank-master.component.ts
+++ src/app/partial/masters/tank-master/tank-master.component.ts
@@ -63,7 +63,7 @@ export class TankMasterComponent implements OnInit {
       "tankName": ['', [Validators.required, Validators.maxLength(100)]],
       "address": ['', [Validators.required, Validators.maxLength(500)]],
       "yojanaId": [this.yojanaArray?.length == 1 ? this.yojanaArray[0].yojanaId : '', [Validators.required]],
-      "networkId": [this.networkArray?.length == 1 ? this.networkArray[0].networkId : '', Validators.required],
+      "networkId": [this.networkArray?.length == 1 && this.getData.userId > 1 ? this.networkArray[0].networkId : '', Validators.required],
       "latitude": [''],
       "longitude": [''],
     })
