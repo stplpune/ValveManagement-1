453e862f98323cf244f64f6bc171cb35a9b2e811
stpl-avinash <avinash.rathod@shauryatechnosoft.com>
2023-03-21 16:27:41 +0530

    dashboard page changes

--- src/app/partial/dashboard/dashboard.component.ts
+++ src/app/partial/dashboard/dashboard.component.ts
@@ -143,52 +143,57 @@ export class DashboardComponent implements OnInit {
 
   waterTankChartData(data?: any) {
     this.chartObj = data;
-    let chartData = data ? [{ "category": data.tankName, "value1": data.percentage, "value2": 100 }] : [{ "category": "", "value1": 0, "value2": 100 }];
+    let chartData = data ? [{ "category": data.tankName, "value1": data.percentage, "value2": 0 }] : [{ "category": "", "value1": 0, "value2": 0 }];
 
     am4core.useTheme(am4themes_animated);
     am4core.addLicense("ch-custom-attribution");
 
     // Create chart instance
-    let chart = am4core.create("valveCylenderChart", am4charts.XYChart3D);
-    chart.titles.create().text = "Water reserves";
+    var chart = am4core.create("valveCylenderChart", am4charts.XYChart);
     chart.data = chartData;
-
-    // Create axes
-    let categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = "category";
     categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.grid.template.strokeOpacity = 0;
+    categoryAxis.renderer.minGridDistance = 30;
+    categoryAxis.renderer.ticks.template.disabled = false;
+    categoryAxis.renderer.ticks.template.strokeOpacity = 0.5;
+    // categoryAxis.title.text = "Water Tank Name";
 
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.renderer.grid.template.strokeOpacity = 0;
-    valueAxis.min = -10;
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
     valueAxis.max = 100;
-    valueAxis.strictMinMax = true;
-    valueAxis.renderer.baseGrid.disabled = true;
+    valueAxis.min = 0;
+    // valueAxis.strictMinMax = true; 
+    valueAxis.calculateTotals = true;
+    valueAxis.title.text = "Water Level In Percentage";
+
     valueAxis.renderer.labels.template.adapter.add("text", function (text: any) {
       if ((text > 100) || (text < 0)) { return ""; }
       else { return text + "%"; }
     })
 
     // Create series
-    let series1 = chart.series.push(new am4charts.ConeSeries());
-    series1.dataFields.valueY = "value1";
-    series1.dataFields.categoryX = "category";
-    series1.columns.template.width = am4core.percent(80);
-    series1.columns.template.fillOpacity = 0.9;
-    series1.columns.template.strokeOpacity = 1;
-    series1.columns.template.strokeWidth = 2;
+    function createSeries(open: any, close: any) {
+      var series: any = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.valueY = close;
+      series.dataFields.openValueY = open;
+      series.dataFields.categoryX = "category";
+      series.clustered = false;
+      // series.strokeWidth = 0;
+      series.columns.template.width = am4core.percent(100);
 
-    let series2 = chart.series.push(new am4charts.ConeSeries());
-    series2.dataFields.valueY = "value2";
-    series2.dataFields.categoryX = "category";
-    series2.stacked = true;
-    series2.columns.template.width = am4core.percent(80);
-    series2.columns.template.fill = am4core.color("#000");
-    series2.columns.template.fillOpacity = 0.1;
-    series2.columns.template.stroke = am4core.color("#000");
-    series2.columns.template.strokeOpacity = 0.2;
-    series2.columns.template.strokeWidth = 2;
+      var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+      labelBullet.label.hideOversized = true;
+      labelBullet.label.fill = am4core.color("#fff");
+      labelBullet.label.adapter.add("text", function (text: any, target: any) {
+        var val = Math.abs(target.dataItem.valueY - target.dataItem.openValueY);
+        return val + '% Water';
+      });
+      labelBullet.locationY = 0.5;
+
+
+    }
+
+    createSeries("value2", "value1");
   }
 
   getTankDeviceHourlyValue() {
@@ -232,7 +237,7 @@ export class DashboardComponent implements OnInit {
     categoryAxis.endLocation = 0.5;
 
     let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    valueAxis.title.text = "Water Percentage";
+    valueAxis.title.text = "Water Level In Percentage";
     valueAxis.calculateTotals = true;
     valueAxis.min = 0;
     valueAxis.max = 100;
